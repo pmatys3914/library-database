66665c08dbaaaa241466ac8720c45311f298d11a
Linnet <Piotr Matys>
2022-02-02 23:19:19 +0100

    Implement adding, viewing and removing from the book table.

--- lib.js
+++ lib.js
@@ -1,6 +1,47 @@
 let http = require('http');
+let url = require('url');
+let mysql = require('mysql');
+let express = require('express');
+let app = express();
+let moment = require('moment');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-}).listen(8080)
+app.get('/', (req, res) => {
+    con.query("SELECT * FROM books", function(err, rows, fields) {
+        if (err) throw err;
+        res.render('site', { title: 'Test', message: 'Test1', items: rows});
+    })
+})
+
+app.get('/add/', (req, res) => {
+    let q = url.parse(req.url, true).query;
+    let sql = "INSERT INTO books (title, author, date_added) VALUES ('" + q.title + "', '" + q.author + "', '" + moment().format('YYYY-MM-DD HH:mm:ss') + "')";
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("1 record added." + moment().format('YYYY-MM-DD HH:mm:ss'));
+    });
+    res.redirect('/');
+})
+
+app.get('/remove/', (req, res) => {
+    let q = url.parse(req.url, true).query;
+    let sql = "DELETE FROM books WHERE id=" + q.removed;
+    con.query(sql, function(err, result) {
+        if(err) throw err;
+        console.log("1 record removed.");
+    });
+    res.redirect('/');
+})
+
+app.listen(3000, () => {
+    console.log('Listening!');
+    app.set('view engine', 'pug');
+})
+
+let con = mysql.createConnection({
+
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Database connection established.");
+})

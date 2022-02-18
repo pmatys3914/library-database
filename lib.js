28cfb87b43b352ad80db23cd63b4f98570e8cd4d
pmatys3914 <piotr.matys.3914@gmail.com>
2022-02-18 20:11:59 +0100

    Refactor main file

--- lib.js
+++ lib.js
@@ -1,22 +1,24 @@
-let http = require('http');
-let url = require('url');
-let mysql = require('mysql');
+//// Require express and configure it
 let express = require('express');
 let app = express();
-app.use(express.static('public'));
-let moment = require('moment');
 
 const bp = require('body-parser');
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
+// Set up pug and public folder
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+// Set up local libraries
 app.locals.moment = require('moment');
 
+// Configure routers
 let indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
 
+// Start listening
 app.listen(3000, () => {
     console.log('Listening!');
-    app.set('view engine', 'pug');
 })

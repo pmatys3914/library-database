e9aac536b3ae4cd244d3fb3f9409206ff99c4d44
pmatys3914 <piotr.matys.3914@gmail.com>
2022-02-18 06:41:45 +0100

    Update Author Controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,38 +2,40 @@ const { body, validationResult } = require("express-validator");
 
 let async = require("async");
 let config = require('../config/databaseConfig');
-let con = config.connection;
+let db = config.connection;
 let moment = require("moment");
 let utils = require("../utils/utils")
 
 // Helper function for rendering the page
 let renderAuthors = function (req, res, msg) {
-    con.query("SELECT * FROM Authors", function (err, rows, fields) {
+    db.query("SELECT * FROM Authors", function (err, rows, fields) {
         if (err) throw err;
-        res.render('authors', { title: 'Authors', items: rows, messages: msg})
+        res.render('authors', { title: 'Authors', items: rows, messages: msg })
     });
 }
 
 // GET index of all authors
-exports.index = function(req, res) {
+exports.index = function (req, res) {
     renderAuthors(req, res, {});
 }
 
 // POST validate and add author
 exports.addAuthor = [
     utils.nameValidator,
-    body('YearBorn', 'Birth Year must not be empty').trim(),
+    body('YearBorn', 'Birth Year must not be empty')
+        .isInt({ max: 2022 })
+        .withMessage("Year needs to be an integer smaller than 2023.")
+        .trim(),
     (req, res, next) => {
         const errors = validationResult(req);
-        if(errors.isEmpty()) {
+        if (errors.isEmpty()) {
             let sql = `INSERT INTO Authors (Name, YearBorn) VALUES (?, ?)`;
-            con.query(sql, [req.body.Name, req.body.YearBorn], function(err, result) {
-                if(err) throw err;
+            db.query(sql, [req.body.Name, req.body.YearBorn], function (err, result) {
+                if (err) throw err;
                 console.log("1 Author added.");
             });
             renderAuthors(req, res, ['Author ' + req.body.Name + ' added to the database.']);
-        } else
-        {
+        } else {
             renderAuthors(req, res, errors.array());
         }
     }
@@ -41,18 +43,46 @@ exports.addAuthor = [
 
 // POST remove author by id
 exports.removeAuthor = [
-    body('Removed', 'Invalid Author').isInt(),
+    body('Removed', 'Invalid Author ID')
+        .isInt()
+        .custom((value) => {
+            return new Promise((resolve, reject) => {
+                let sql = `SELECT * FROM Authors WHERE AuthorID = ?`;
+                db.query(sql, value, (err, rows) => {
+                    if (err) {
+                        reject(new Error(err));
+                    }
+                    if (rows.length == 0) {
+                        reject(new Error(`Invalid Author ID.`));
+                    }
+                    resolve(true);
+                });
+            })
+        })
+        .custom((value) => {
+            return new Promise((resolve, reject) => {
+                let sql = `SELECT BookID, Title FROM Books WHERE AuthorID = ?`;
+                db.query(sql, value, (err, rows) => {
+                    if (err) {
+                        reject(new Error(err));
+                    }
+                    if (rows.length != 0) {
+                        reject(new Error(`Can't remove an author who still has books in the database.`));
+                    }
+                    resolve(true);
+                });
+            })
+        }),
     (req, res, next) => {
         const errors = validationResult(req);
-        if(errors.isEmpty()) {
+        if (errors.isEmpty()) {
             let sql = 'DELETE FROM Authors WHERE AuthorID = ?';
-            con.query(sql, [req.body.Removed], function(err, result) {
+            db.query(sql, [req.body.Removed], function (err, result) {
                 if (err) throw err;
                 console.log("1 Author removed.");
             });
             renderAuthors(req, res, ['Author removed from the database.']);
-        } else
-        {
+        } else {
             renderAuthors(req, res, errors.array());
         }
     }
@@ -60,20 +90,34 @@ exports.removeAuthor = [
 
 // POST validate and edit author
 exports.editAuthor = [
-    body('AuthorID', 'Invalid Author').isInt(),
+    body('AuthorID', 'Invalid Author ID')
+        .isInt()
+        .custom((value) => {
+            return new Promise((resolve, reject) => {
+                let sql = `SELECT * FROM Authors WHERE AuthorID = ?`;
+                db.query(sql, value, (err, rows) => {
+                    if (err) {
+                        reject(new Error(err));
+                    }
+                    else if (rows.length == 0) {
+                        reject(new Error(`Invalid Author ID.`));
+                    }
+                    resolve(true);
+                });
+            })
+        }),
     utils.nameValidator,
-    body('YearBorn', 'Birth Year must not be empty').trim().isLength({ min: 1}).escape(),
+    body('YearBorn', 'Birth Year must not be empty').trim().isLength({ min: 1 }).escape(),
     (req, res, next) => {
         const errors = validationResult(req);
-        if(errors.isEmpty()) {
+        if (errors.isEmpty()) {
             let sql = 'UPDATE Authors SET Name= ?, YearBorn= ? WHERE AuthorID= ?';
-            con.query(sql, [req.body.Name, req.body.YearBorn, req.body.AuthorID], function(err, result) {
+            db.query(sql, [req.body.Name, req.body.YearBorn, req.body.AuthorID], function (err, result) {
                 if (err) throw err;
                 console.log("1 Author updated.");
             });
             renderAuthors(req, res, ['Author ' + req.body.Name + ' updated.']);
-        } else
-        {
+        } else {
             renderAuthors(req, res, errors.array());
         }
     }

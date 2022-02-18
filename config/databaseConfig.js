d06b5dc7f3ee6a213e27b38784d336e5fc7408ed
pmatys3914 <piotr.matys.3914@gmail.com>
2022-02-18 20:11:59 +0100

    Fixed commits

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -13,8 +13,13 @@ connection.connect(function(err) {
     }
 
     // Check if the database has all required tables. If not, create them in place of existing ones.
-    let testsql = "SELECT * FROM information_schema.tables WHERE table_schema = 'library' AND (table_name = 'Books' OR table_name = 'Authors')";
-    let reqtables = 2;
+    let reqtables = ["Books", "Authors", "BookInstances"];
+    let testsql = "SELECT * FROM information_schema.tables WHERE table_schema = 'library' AND (";
+    reqtables.forEach(e => testsql += ( "table_name = \'" + e + "\' OR "));
+    testsql = testsql.slice(0, -3);
+    testsql += ")";
+    console.log(testsql);
+
     connection.query(testsql, function(err, rows) {
         if(err)
         {
@@ -23,15 +28,17 @@ connection.connect(function(err) {
         }
         else
         {
-            if(rows.length == reqtables)
+            if(rows.length == reqtables.length)
             {
                 console.log("Database verified.");
             }
             else
             {
-                console.log("Building database." + rows.length);
+                console.log("Building database.");
                 // Drop all the conflicting tables if they exists
-                let dropsql = "DROP TABLE IF EXISTS Authors, Books";
+                let dropsql = "DROP TABLE IF EXISTS ";
+                reqtables.forEach(e => dropsql += e + ", ");
+                dropsql = dropsql.slice(0, -2);
                 connection.query(dropsql, function(err) {
                     if(err)
                     {
@@ -43,7 +50,7 @@ connection.connect(function(err) {
                     CREATE TABLE Authors (
                         AuthorID int NOT NULL AUTO_INCREMENT,
                         Name varchar(255) NOT NULL,
-                        YearBorn int NOT NULL,
+                        YearBorn year NOT NULL,
                         PRIMARY KEY(AuthorID)
                     )`;
                     connection.query(authorssql, function(err) {
@@ -62,6 +69,18 @@ connection.connect(function(err) {
                         FOREIGN KEY (AuthorID) REFERENCES Authors(AuthorID)
                     )`;
                     connection.query(bookssql, function(err) {
+                        if(err) throw err;
+                    });
+                    // Book Instances
+                    let bookinstancesql = `
+                    CREATE TABLE BookInstances (
+                        BookInstanceID int NOT NULL AUTO_INCREMENT,
+                        BookID int NOT NULL,
+                        AcquisitionDate timestamp NOT NULL,
+                        PRIMARY KEY (BookInstanceID),
+                        FOREIGN KEY (BookID) REFERENCES Books(BookID)
+                    )`;
+                    connection.query(bookinstancesql, function(err) {
                         if(err) throw err;
                     });
                 })
